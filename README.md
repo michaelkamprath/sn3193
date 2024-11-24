124b421ac11a63da09a9ab99e02d8ca01a62282c
Michael Kamprath <michael@kamprath.net>
2024-11-23 19:47:17 -0800

    started documentation

---
+++ README.md
@@ -0,0 +1,10 @@
+# SN3193 Driver for Rust
+<!-- cargo-sync-readme start -->
+
+This Rust `embedded-hal`-based library is a simple way to control the SN3193 RGB LED driver.
+The SN3193 is a 3-channel LED driver with PWM control and breathing mode. It can drive up to
+3 LEDs with a maximum current of 42 mA and is controlled via I2C. This driver is designed for the
+`no_std` environment, so it can be used in embedded systems.
+
+
+<!-- cargo-sync-readme end -->
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! This Rust `embedded-hal`-based library is a simple way to control the SN3193 RGB LED driver.
+//! The SN3193 is a 3-channel LED driver with PWM control and breathing mode. It can drive up to
+//! 3 LEDs with a maximum current of 42 mA and is controlled via I2C. This driver is designed for the
+//! `no_std` environment, so it can be used in embedded systems.
+//! 
+
 #![no_std]
 #![allow(dead_code, clippy::unusual_byte_groupings)]
 use embedded_hal::{delay::DelayNs, i2c};
